be4567d330fc966ec29ac31bd227e8fda2523b72
Ryan Ming <ryan.ming@gmail.com>
2013-11-15 16:51:27 -0500

    removed factories for demo

--- test/factories/post_factory.rb
+++
@@ -1,9 +0,0 @@
-FactoryGirl.define do
-  factory :post do
-    title "amazing post title!!!1111"
-    content "it was the best of times, it was the blurst of times..."
-    description "a heartbreaking work of staggering genius"
-
-    user
-  end
-end
