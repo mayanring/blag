8364c5c37219b0a55b40c35c97587b90b546c6f0
Ryan Ming <ryan.ming@gmail.com>
2013-11-15 16:24:38 -0500

    added post and user factories

---
+++ test/factories/post_factory.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :post do
+    title "amazing post title!!!1111"
+    content "it was the best of times, it was the blurst of times..."
+    description "a heartbreaking work of staggering genius"
+  end
+end
