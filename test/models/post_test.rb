9aa759a83502222b721511a1c9322cd4ab35d236
Ryan Ming <ryan.ming@gmail.com>
2013-11-15 16:48:33 -0500

    removed passing test from post_test.rb

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -2,12 +2,10 @@ require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
   test "post is invalid without title" do
-      post = FactoryGirl.build( :post, :title => nil )
       assert !post.valid?
   end
 
   test "post is invalid without content" do
-    post_without_content = FactoryGirl.build( :post, :content => nil)
     assert !post_without_content.valid?
   end
 end
